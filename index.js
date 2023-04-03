1ea750039987ebd8a068b7d4f5b47c56f90eeb05
codingscode <heronnestg@hotmail.com>
2023-04-03 11:54:48 -0300

    parte 3

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 
 // config inicial
 const express = require('express')
+
 const app = express()
 const mongoose = require('mongoose')
 
+const Person = require('./models/Person')
 
 
 // forma de ler JSON
@@ -14,6 +16,36 @@ app.use(
 )
 
 app.use(express.json())
+
+// rotas da API
+app.post('/person', async (req, res) => {
+   
+   // req.body
+   const { name, salary, approved } = req.body
+   
+   // {name: 'Matheus', salary: 5000, approved: false}
+
+   if (!name) {
+      res.status(422).json({ error: 'O nome é obrigatório' })
+   }
+   
+   const person = { name, salary, approved }
+   // create
+   
+   try {
+      // criando dados
+      await Person.create(person)
+      
+      res.status(201).json({ message: 'Pessoa inserida com sucesso!' })
+      
+   }
+   catch(error) {
+      res.status(500).json({ error: error })
+   }
+   
+   
+})
+
 
 // rota inicial / endpoint
 app.get('/', (req, res) => {
@@ -28,7 +60,7 @@ app.get('/', (req, res) => {
 const DB_USER = 'hp'
 const DB_PASSWORD = encodeURIComponent('AC1eaLzMIROybwVB')
 
-mongoose.connect('mongodb://localhost:27017/?directConnection=true')
+mongoose.connect('mongodb://localhost:27017/exemploapi?directConnection=true')
 .then( () => {
    console.log('Conectamos ao mongodb!')
    
@@ -36,11 +68,6 @@ mongoose.connect('mongodb://localhost:27017/?directConnection=true')
    app.listen(3000)
 } )
 .catch( (err) => console.log(err) )
-
-
-//app.listen(3000)
-
-//mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.xlcvivo.mongodb.net/bancoapi?retryWrites=true&w=majority`)
 
 
 

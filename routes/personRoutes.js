314215d8e549c5bb252cbebab008d1734e320bb6
codingscode <heronnestg@hotmail.com>
2023-04-04 11:14:52 -0300

    parte 11

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -94,9 +94,33 @@ router.patch('/:id', async (req, res) => {
       res.status(500).json({ error: err })
    }
    
+})
+
+// delete - deletar dados
+router.delete('/:id', async (req, res) => {
+
+   const id = req.params.id
    
+   const person = await Person.findOne({ _id: id })
+   
+   if (!person) {
+      res.status(422).json({ message: 'O usuário não foi encontrado!' })
+      return
+   }
+   
+   try {
+      await Person.deleteOne({ _id: id })
+      
+      res.status(200).json({ message: 'Usuário removido com sucesso!' })
+   }
+   catch (err) {
+      res.status(500).json({ error: err })
+   }
+   
+
 })
 
 
 module.exports = router
+
 
